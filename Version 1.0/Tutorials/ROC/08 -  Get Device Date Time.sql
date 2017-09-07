74163dcaf3136c3c5fb3531eaf027792b61e412f
Jason Craig <jason.craig@outlook.com>
2017-09-06 22:34:46 -0600

    Typo

--- Version 1.0/Tutorials/ROC/08 -  Get Device Date Time.sql
+++ Version 1.0/Tutorials/ROC/08 -  Get Device Date Time.sql
@@ -2,7 +2,7 @@ USE [DeviceSQL]
 GO
 DECLARE @deviceName nvarchar(512) = 'FB107-01';
 DECLARE @century int = 2000; 
-SELECT [ROCMaster].[GetDateTimeWithCentury] (@deviceName, @century)
+SELECT [ROCMaster].[GetRealTimeClockValueWithCentury] (@deviceName, @century)
 
 
 
