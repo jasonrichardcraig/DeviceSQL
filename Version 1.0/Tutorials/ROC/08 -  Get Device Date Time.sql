2282a323a8cbeea871c78e2585d0f5cd8662089c
Jason Craig <jason.craig@outlook.com>
2017-09-06 22:42:41 -0600

    TODO: Add MODBUS tutorials

--- Version 1.0/Tutorials/ROC/08 -  Get Device Date Time.sql
+++ Version 1.0/Tutorials/ROC/08 -  Get Device Date Time.sql
@@ -1,8 +1,8 @@
 USE [DeviceSQL]
 GO
-DECLARE @deviceName nvarchar(512) = 'FB107-01';
+DECLARE @deviceName nvarchar(512) = 'FB103-01';
 DECLARE @century int = 2000; 
-SELECT [ROCMaster].[GetDateTimeWithCentury] (@deviceName, @century)
+SELECT [ROCMaster].[GetRealTimeClockValueWithCentury] (@deviceName, @century)
 
 
 
